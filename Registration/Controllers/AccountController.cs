32dbc32e4118c5f47dc8b490bc62433c47f5ab3d
Alex <akaftann@gmail.com>
2018-01-15 22:11:12 +0200

    Добавьте файлы проекта.

---
+++ Registration/Controllers/AccountController.cs
@@ -0,0 +1,88 @@
+﻿using Registration.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Registration.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                //search usern in bd
+                User user = null;
+                using (UserContext db = new UserContext())
+                {
+                    user = db.Users.FirstOrDefault(u => u.Email == model.Name && u.Password == model.Password);
+                }
+                if(user != null)
+                {
+                    FormsAuthentication.SetAuthCookie(model.Name, true);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "User not found");
+                }
+            }
+            return View(model);
+        }
+
+
+
+
+
+        
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                User user = null;
+                using (UserContext db = new UserContext())
+                {
+                    user = db.Users.FirstOrDefault(u => u.Email == model.Name);
+                }
+                if(user == null)
+                {
+                    //Create new user
+                    using (UserContext db = new UserContext())
+                    {
+                        db.Users.Add(new Models.User { Email = model.Name, Password = model.Password, Age = model.Age, RoleId = 2 });
+                        db.SaveChanges();
+
+                        user = db.Users.Where(u => u.Email == model.Name && u.Password == model.Password).FirstOrDefault();
+                    }
+                    // if users add succefull
+                    if(user != null)
+                    {
+                        FormsAuthentication.SetAuthCookie(model.Name, true);
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "This Email does already exist");
+                }
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Registration/Models/Models.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Registration.Models
+{
+    public class LoginModel
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class RegisterModel
+    {
+        public string Name { get; set; }
+        public  string Password { get; set; }
+        public int Age { get; set; }
+    }
+    
+    
+}
